bf72ed8f4b740bdc1e19800f060a7f350e94ff46
Manu <manuel.brusche@web.de>
2023-03-09 17:50:21 +0100

    removed personal and working-files from project

--- src/main/java/com/manu/GradeR/GradeRApplication.java
+++ src/main/java/com/manu/GradeR/GradeRApplication.java
@@ -1,13 +1,7 @@
 package com.manu.GradeR;
 
-import com.manu.GradeR.SchoolClass.SchoolClassRepository;
-import com.manu.GradeR.model.User;
-import com.manu.GradeR.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class GradeRApplication {
@@ -16,14 +10,4 @@ public class GradeRApplication {
 		SpringApplication.run(GradeRApplication.class, args);
 	}
 
-
-	@Bean
-	CommandLineRunner commandLineRunner(UserRepository users, SchoolClassRepository schoolClassRepository, PasswordEncoder passwordEncoder) {
-		return args -> {
-			users.save(new User("u1", passwordEncoder.encode("u1"), "ROLE_TEACHER"));
-			users.save(new User("u2", passwordEncoder.encode("u2"), "ROLE_TEACHER"));
-			users.save(new User("u3", passwordEncoder.encode("u3"), "ROLE_TEACHER"));
-
-		};
-	}
 }
